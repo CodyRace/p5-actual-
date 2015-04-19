8103eb7fa2e4846f8826f73f64d84db217819ff3
Cody McLean Race <crace@onyx.boisestate.edu>
2015-04-19 17:20:10 -0600

    thread random updated

---
+++ thread-random.c
@@ -0,0 +1,60 @@
+
+//TODO: Use a basically sec-random program and update it w/ additional functionality
+//
+//Copy-Pasted from seq-random.c
+//Some code from thread-ids.c
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>  // must include stdio  for pvm3.h to compile correctly
+#include <sys/times.h> /* for times system call */
+#include <sys/time.h>  /* for gettimeofday system call */
+#include <pthread.h>
+
+/*#define DEBUG 1*/
+
+#define RANDOM_SEED   12345678 
+
+//The main work routine
+void generateRandomNumbers(long long);  
+double getMilliSeconds();
+
+
+/* The main work routine */
+void generateRandomNumbers(long long int count)  
+{
+	long long int i;
+	long int x;
+
+	srandom(RANDOM_SEED);
+    for (i = 0; i < count; i++) {
+		x = random();
+		printf("%ld\n", x);
+  	}
+}
+
+//TODO replace count with the incementing thread ids gotten from argv[2]
+int main(int argc, char **argv) 
+{
+
+	long long int count;
+
+    double timeStart = 0;
+    double timeElapsed = 0;
+
+  	if (argc < 2) {
+        fprintf(stderr, "Usage: %s <n>\n" ,argv[0]);
+        exit(1);
+    }
+    sscanf(argv[1],"%lld",&count); /* lld for long long int */
+
+    timeStart = getMilliSeconds();	//And we are off
+    generateRandomNumbers(count);
+	printf("generated %lld random numbers\n", count);
+
+    timeElapsed = getMilliSeconds() - timeStart;
+    printf("Elapsed time:  %lf seconds\n",(double)(timeElapsed/1000.0));
+    fflush(stdout);
+
+    exit(0);
+}
