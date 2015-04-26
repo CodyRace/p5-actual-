8d98efcc318ae440a61a58c2f939305ef680df7f
Cody McLean Race <crace@onyx.boisestate.edu>
2015-04-26 15:44:31 -0600

    Working Program, but has serious memory leaks

--- thread-random.c
+++ thread-random.c
@@ -12,40 +12,67 @@
 #include <pthread.h>
 
 /*#define DEBUG 1*/
-
+#define BUFSIZE 32
 #define RANDOM_SEED   12345678 
 
 //The main work routine
-void generateRandomNumbers(long long);  
+void generateRandomNumbers(long long, int);  
 double getMilliSeconds();
 long long int threadcount;
 long long int randomcount;
 
 
 /* The main work routine */
-void generateRandomNumbers(long long int count)  
+void generateRandomNumbers(long long int count, int threadid)  //use threadid as the seed
 {
+//int cnt;
+//cnt = ((int*)threadid);
+
+
   int i;
 	long int x;
 
-	srandom(RANDOM_SEED);
-    for (i = 0; i < count; i++) {
-		x = random();
-		printf("%ld\n", x);
+//	random_r(threadid);
+//    for (i = 0; i < count; i++) {
+//		  x = random();
+//		  printf("%ld\n", x);
+//  	}
+   
+    struct random_data *rdata = (struct random_data *) malloc(sizeof(struct random_data));
+    char *statebuf = (char*) malloc(sizeof(char) * BUFSIZE);
+
+    int32_t value;
+
+    initstate_r(threadid, statebuf, BUFSIZE, rdata);
+    int status = random_r (rdata, &value);
+    if(status != 0)
+    {
+        perror(" random_r");
+        exit(status);
+    }
+      for (i = 0; i < count; i++) {
+		  x = random();
+		  printf("TID:%d  %ld\n",threadid, x);
   	}
+
+
 }
 
 void *run(void *ptr)//call genearateRandNum~~ here
 {
+int id;
+id = *((int*)ptr);
 
-
+printf("  Thread %d is generating %lld random numbers\n",id, randomcount);
+    generateRandomNumbers(randomcount, id);
+   //   printf("  generated %lld random numbers\n", randomcount);
 	pthread_exit(NULL);
 }
 
 //TODO replace count with the incementing thread ids gotten from argv[2]
 int main(int argc, char **argv) 
 {
-	int i, n;
+	int i;//, n;
 	int *value;
     pthread_t *tid; 
  
@@ -59,19 +86,31 @@ int main(int argc, char **argv)
  //   sscanf(argv[1],"%lld",&count); /* lld for long long int */
     threadcount = atoi(argv[2]);
     randomcount = atoi(argv[1]);
+    timeStart = getMilliSeconds();	//And we are off (START TIMER)
+    tid = (pthread_t *) malloc(sizeof(pthread_t) * threadcount);
     for (i = 0; i < threadcount; i++){
-        generateRandomNumbers(randomcount);
-        printf("generated %lld random numbers\n", randomcount);
-
-    }
     
-    timeStart = getMilliSeconds();	//And we are off
+    		value = (int *) malloc(sizeof(int));
+	    	*value = i;
+                //the value must be made or i will be read incorrectly
+        pthread_create(&tid[i], NULL, run, (void *) value); 
+       pthread_join(tid[i], NULL);
+
+  	}
+//   	for (i=0; i<threadcount; i++){
+//     	pthread_join(tid[i], NULL);
+//  }
+
+
+
+    
+
 //  generateRandomNumbers(count);
 //  printf("generated %lld random numbers\n", count);
 
     timeElapsed = getMilliSeconds() - timeStart;
     printf("Elapsed time:  %lf seconds\n",(double)(timeElapsed/1000.0));
     fflush(stdout);
-
-    exit(0);
+//    pthread_exit(NULL);
+    exit(EXIT_SUCCESS);
 }
